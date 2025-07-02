733ef2a291dbcf0c6e3ae2182c0b8aa740cb7d90
mapadevsports <mapa.dev.sports@gmail.com>
2025-07-02 11:52:58 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ echo "Iniciando aplicação UWB API..."
 
 # As tabelas do banco de dados serão criadas/gerenciadas manualmente via pgAdmin ou migrações
 # python src/create_db.py  # Esta linha pode ser removida ou comentada
-
+flask init-db
 # Iniciar aplicação com Gunicorn
 exec gunicorn --config gunicorn.conf.py src.main:app
